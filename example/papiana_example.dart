de84e9cfafbd8e4ffdcc6243aea8a824164fe54a
xavierchanth <xchanthavong@gmail.com>
2022-08-09 17:19:20 +0800

    fix: Reset package

--- example/papiana_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:papiana/papiana.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
--- lib/papiana.dart
+++ lib/papiana.dart
@@ -2,7 +2,3 @@
 ///
 /// More dartdocs go here.
 library papiana;
-
-export 'src/papiana_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
